5ea872350b26e98f1bbd62e7c33c9b30dd9ce6ba
Kuvshinov Vlad <kuvshinov_v@mail.ru>
2022-09-13 21:08:38 +0300

    Добавил решение второй задачи № 56

--- Program.cs
+++ Program.cs
@@ -1,4 +1,27 @@
-﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+﻿
+int[,] CreateRandom2DArray(int rows, int cols, int min, int max){
+    int[,] array = new int[rows, cols];
+    for(int i = 0 ; i <rows; i++){
+        for(int j = 0; j<cols; j++){
+            array[i,j] = new Random().Next(min,max+1);
+        }
+    }
+    return array;
+}
+
+void Show2dArray(int[,] array){
+    for (int i = 0; i < array.GetLength(0); i++)
+    {   Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j]+" ");
+        }
+    }
+}
+
+
+
+// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
@@ -23,6 +46,75 @@
 // 8 4 2 4
 // 5 2 6 7
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+int[] MinSummInRow(int[,] array){
+    int[] summ = new int[array.GetLength(0)];
+    
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {  
+            for (int k = 0; k < array.GetLength(0); k++)
+            {   
+                if (i == k)
+                {  
+                    summ[k] = summ[k] + array[i,j];
+                  
+                    if(j == array.GetLength(0)-1){     
+                        Console.WriteLine("Сумма = "+summ[k]+"  "+k+" строка");
+                           
+                    }
+                }
+            }
+        }
+    }
+    return summ;
+}
+
+void MinEL(int[] array){
+    int minElement = array[0];
+    int stroka = 0;
+        for (int i = 0; i <= array.Length-1; i++) {
+            if (array[i] <= minElement){
+                minElement = array[i];   //минимальный элемент
+                stroka = i;  // номер строки в которой он  
+                
+            }
+        } 
+    Console.WriteLine($"Минимальная сумма {minElement} находится в {stroka} строке ");
+}
+
+
+
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int cols = Convert.ToInt32(Console.ReadLine());
+if (rows == cols)
+{
+    Console.Write("Введите значение минимального элемента: ");
+    int min = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите число максимального элемента: ");
+    int max = Convert.ToInt32(Console.ReadLine());
+
+    int[,] array = CreateRandom2DArray(rows, cols, min, max);
+    Show2dArray(array); 
+    Console.WriteLine();
+    int[] minnn = MinSummInRow(array);
+    //ShowArray(minnn);
+    Console.WriteLine();
+    MinEL(minnn);
+    //Console.WriteLine("Минимальная сумма в "+minnn+" строке");
+}
+else
+{
+    Console.WriteLine("Введён не прямоугольный двумерный массив!");
+}
+
+
+
+
 
 
 
